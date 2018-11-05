54fd6aaafff6a0c756be4cedba3d68edfd15f501
waffenddy <waffenddy@yahoo.com>
2018-11-05 15:17:21 +0800

    Added function

--- register.php
+++ register.php
@@ -1,5 +1,28 @@
 <?php
 
+	function sanitizeFormPassword($inputText) {
+		$inputText = strip_tags($inputText);
+		return $inputText;
+	}
+	
+
+	function sanitizeFormUsername($inputText) {
+
+		$inputText = strip_tags($inputText);
+		$inputText = str_replace(" ","", $inputText);
+		return $inputText;
+	}
+
+	function sanitizeFormString($inputText) {
+
+		$inputText = strip_tags($inputText);
+		$inputText = str_replace(" ","",$inputText);
+		$inputText = ucfirst(strtolower($inputText));
+		return $inputText;
+	}
+
+
+
 	if (isset($_POST['loginButton'])) {
 
 		//echo "Login button was pressed";
@@ -10,6 +33,14 @@
 	if (isset($_POST['registerButton]')) {
 
 	       // echo "Register button was pressed";
+
+		$username = sanitizeFormUsername ($_POST['username']);
+		$firstname = sanitozeFormString(['firstname']);
+		$lastname = sanitozeFormString(['lastname']);
+		$email = sanitozeFormString(['email']);
+		$email2 = sanitozeFormString(['email2']);
+		$password = sanitozeFormPassword(['password']);
+		$password2 = sanitozeFormPassword(['password2']);
 
 	}
 
